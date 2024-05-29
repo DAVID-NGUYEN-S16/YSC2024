d88c08d8ec0da511614881708f030bd8c8e72fb1
DAVID-NGUYEN-S16 <nguyenvannam14056969@gmail.com>
2024-05-30 04:54:23 +0700

    ultis

--- utils.py
+++ utils.py
@@ -5,3 +5,16 @@ def preprocess_text(text: str) -> str:
     text = " ".join(text.split())
     text = text.lower()
     return text
+from omegaconf import OmegaConf
+import json
+
+def write_json(path, data):
+    with open(path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)  
+
+def load_config(file_path):
+    """
+    Load a YAML configuration file using OmegaConf.
+    """
+    config = OmegaConf.load(file_path)
+    return config
